d6faf95f4f23d2ff4031b1d741737131c768b6e8
Timur Shafigullin <timur.shafigullin@flatstack.com>
2019-06-22 19:44:41 +0300

    Localize receipt manager

--- Sources/App/Tools/Receipt/DefaultReceiptManager.swift
+++ Sources/App/Tools/Receipt/DefaultReceiptManager.swift
@@ -35,9 +35,9 @@ class DefaultReceiptManager: ReceiptManager {
             } else if response.http.status == .notAcceptable {
                 return false
             } else if response.http.status == .badRequest {
-                throw Abort(.badRequest, reason: "Missing required parameters")
+                throw Abort(.badRequest, reason: "Missing required parameters".localized(on: request))
             } else {
-                throw Abort(.badRequest, reason: "Unknown error from Federal Tax Service")
+                throw Abort(.badRequest, reason: "Unknown error from Federal Tax Service".localized(on: request))
             }
         }
     }
@@ -48,7 +48,7 @@ class DefaultReceiptManager: ReceiptManager {
         let url = "https://proverkacheka.nalog.ru:9999/v1/inns/*/kkts/*/fss/\(form.fn)/tickets/\(form.fd)?fiscalSign=\(form.fiscalSign)&sendToEmail=no"
 
         guard let login = Environment.FTS_LOGIN, let password = Environment.FTS_PASSWORD else {
-            throw Abort(.internalServerError, reason: "Missing login/password for Federal Tax Service")
+            throw Abort(.internalServerError, reason: "Missing login/password for Federal Tax Service".localized(on: request))
         }
 
         let loginPassword = "\(login):\(password)"
@@ -66,13 +66,13 @@ class DefaultReceiptManager: ReceiptManager {
                     return rawReceipt.document.receipt
                 }
             } else if response.http.status == .forbidden {
-                throw Abort(.forbidden, reason: "Login or password for Federal Tax Service incorrect")
+                throw Abort(.forbidden, reason: "Login or password for Federal Tax Service incorrect".localized(on: request))
             } else if response.http.status == .notAcceptable {
-                throw Abort(.badRequest, reason: "Check not found")
+                throw Abort(.badRequest, reason: "Check not found".localized(on: request))
             } else if response.http.status == .accepted {
-                throw Abort(.badRequest, reason: "Check accepted")
+                throw Abort(.badRequest, reason: "Check accepted".localized(on: request))
             } else {
-                throw Abort(.badRequest, reason: "Unknown error from Federal Tax Service")
+                throw Abort(.badRequest, reason: "Unknown error from Federal Tax Service".localized(on: request))
             }
         }
     }
